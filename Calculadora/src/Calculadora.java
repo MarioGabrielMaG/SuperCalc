048bb723a3b42272d0ee7e1c193e82b74fa3ade2
Juan Álvarez Manzano <juantecno180@gmail.com>
2021-10-22 20:57:21 +0200

    22/10/2021 20:56
    Añadidos ambos menús el switch para acceder a ellos y el switch para acceder a las diferentes operaciones.

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -23,11 +23,206 @@ public class Calculadora {
 	 * @param args
 	 */
 	public static void main(String[] args) {	
-		System.out.println("");
-		System.out.println(" 1 - Operaciones  2 - Ayuda ");
-		System.out.println("");
+		System.out.println("***********************************");
+		System.out.println("* A -Lista Operaciones  B - Ayuda *");
+		System.out.println("*                o                *");
+		System.out.println("*    -Introduce una operación-    *");
+		System.out.println("***********************************");
 		op = inp.next();
 		System.out.print(op);
+		switch (op.toUpperCase()){
+		
+		case "A":
+			ListaOperaciones();
+			break;
+			
+		case "B": 
+			Ayuda();
+			break;
+		default:
+			Operaciones();
+			
+		
+		
+		}
+		
 	}
+	public static void ListaOperaciones() { //menú de operaciones
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Las operaciónes disponibles son:");
+		System.out.println("---------------------------------");
+		System.out.println("*Suma (+)");
+		System.out.println("*Resta (-)");
+		System.out.println("*Multiplicación (*)");
+		System.out.println("*División (/)");
+		System.out.println("*Módulo (%)");
+		System.out.println("*Raiz Cuadrada (sqr)");
+		System.out.println("*Factorial (!)");
+		System.out.println("*Potencia (^)");
+		System.out.println("*Seno (SEN)");
+		System.out.println("*Coseno (COS)");
+		System.out.println("*Tangente (TAN)");
+		System.out.println("*Arcoseno (ARCSEN)");
+		System.out.println("*Arcocoseno (ARCOS)");
+		System.out.println("*Mayor número (>)");
+		System.out.println("*Menor número (<)");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("*Para más información sobre la sintaxis y uso de las operaciónes dirijasé al menú de ayuda*");
+		
+	}
+	public static void Ayuda() {	//menú de ayuda
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Ayuda sobre el uso de las operaciónes:");
+		System.out.println("---------------------------------------");
+		System.out.println("*Suma (A+B= Suma A más B)");
+		System.out.println("*Resta (A-B= Resta A menos B)");
+		System.out.println("*Multiplicación (A*B= Multiplicación A por B)");
+		System.out.println("*División (A/B= División A entre B)");
+		System.out.println("*Módulo (A%B= Módulo de A sobre B)");
+		System.out.println("*Raiz Cuadrada (A sqr= raiz cuadrada de A)");
+		System.out.println("*Factorial (!A = Multiplicación de todos los números hasta A)");
+		System.out.println("*Potencia (A^B = Resultado de A elevado a B)");
+		System.out.println("*Seno (A SEN = Seno de A  )");
+		System.out.println("*Coseno (A COS = Coseno de A)");
+		System.out.println("*Tangente (A TAN = Tangente de A)");
+		System.out.println("*Arcoseno (A ARCSEN = Arcseno de A)");
+		System.out.println("*Arcocoseno (A ARCOS = Arcoseno de A)");
+		System.out.println("*Mayor número (A>B = mayor número entre A y B)");
+		System.out.println("*Menor número (A<B = menor número entre A y B)");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("*Vuelva al menú para operar*");
+		
+		
+	}
+	public static void Operaciones() {	//filtramos la operación recibida por el usuario
+		
+		switch (op) {
+		
+			case "+":
+				suma();
+				break;
+			case "-":
+				resta();
+				break;
+			case "*":
+				multiplicacion();
+				break;
+			case "/":
+				división();
+				break;
+			case "%":
+				modulo();
+				break;
+			case "sqrt":
+				raiz();
+				break;
+			case "!":
+				factorial();
+				break;
+			case "^":
+				potencia();
+				break;
+			case "SEN":
+				seno();
+				break;
+			case "COS":
+				coseno();
+				break;
+			case "TAN":
+				tangente();
+				break;
+			case "ARCSEN":
+				arcseno();
+				break;
+			case "ARCOS":
+				arcos();
+				break;
+			case ">":
+				mayornum();
+				break;
+			case "<":
+				menornum();
+				break;
+			default:
+				
+				
+				
+				
+				
+				
+		
+		
+		
+		}
+		
+	}
+	public static void menornum() {
+		
+		
+	}
+	public static void mayornum() {
+		
+		
+	}
+	public static void arcos() {
+		
+		
+	}
+	public static void arcseno() {
+		
+		
+	}
+	public static void tangente() {
+		
+		
+	}
+	public static void coseno() {
+		
+		
+	}
+	public static void seno() {
+		
+		
+	}
+	public static void potencia() {
+		
+		
+	}
+	public static void factorial() {
+		
+		
+	}
+	public static void raiz() {
+		
+		
+	}
+	public static void modulo() {
+		
+		
+	}
+	public static void división() {
+		
+		
+	}
+	public static void multiplicacion() {
+		
+		
+	}
+	public static void resta() {
+		
+		
+	}
+	public static void suma() {
+		
+		
+		
+	}
+	
 
 }
